8870370f9e3333b5cca6dfe49743e89f864329aa
Stella <stella.jin123@gmail.com>
2024-10-16 13:33:59 +0200

    update header and nav

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/Header";
 import React from "react";
 
 type Props = {
@@ -5,7 +6,12 @@ type Props = {
 };
 
 function Layout({ children }: Props) {
-  return <div>{children}</div>;
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="container mx-auto flex-1 py-10">{children}</div>
+    </div>
+  );
 }
 
 export default Layout;
