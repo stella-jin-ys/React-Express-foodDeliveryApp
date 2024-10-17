02ca8143b2144ac5f2980cd3df7978db42c84309
Stella <stella.jin123@gmail.com>
2024-10-17 21:14:23 +0200

    update homepage

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,12 @@
+function Hero() {
+  return (
+    <div>
+      <img
+        src="https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg"
+        className="w-full max-h-[600px] object-cover"
+      />
+    </div>
+  );
+}
+
+export default Hero;
