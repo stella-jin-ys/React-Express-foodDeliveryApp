41996e52f688be3c1b9a95ad649a5d4ee13562db
Stella <stella.jin123@gmail.com>
2024-10-22 14:07:32 +0200

    login with auth0

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import AppRoutes from "./AppRoutes";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
-      <AppRoutes />
+      <Auth0ProviderWithNavigate>
+        <AppRoutes />
+      </Auth0ProviderWithNavigate>
     </Router>
   </StrictMode>
 );
