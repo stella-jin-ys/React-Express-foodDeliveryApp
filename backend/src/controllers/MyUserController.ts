5a2e13ad5e3e2d8416934ff7c1900288dae4e517
Stella <stella.jin123@gmail.com>
2024-10-23 23:11:19 +0200

    create user route

---
+++ backend/src/controllers/MyUserController.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+import User from "../models/user";
+
+const createCurrentUser = async (req: Request, res: Response) => {};
+
+export default {
+  createCurrentUser,
+};
