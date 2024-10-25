6eb86f332671b9de560d8ff8188bcfcc6de8dc49
Stella <stella.jin123@gmail.com>
2024-10-25 14:26:54 +0200

    create user api

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -1,7 +1,20 @@
 import { Request, Response } from "express";
 import User from "../models/user";
 
-const createCurrentUser = async (req: Request, res: Response) => {};
+const createCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const { auth0Id } = req.body;
+    const existingUser = await User.findOne({ auth0Id });
+    if (existingUser) {
+      return res.status(200).json(existingUser);
+    }
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(201).json(newUser.toObject());
+  } catch (error) {
+    res.status(500).json({ message: "Error creating user", error });
+  }
+};
 
 export default {
   createCurrentUser,
