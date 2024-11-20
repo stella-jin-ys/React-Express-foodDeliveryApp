dddd299023b7966da0bf28de69f5c8f10bc466d8
Stella <stella.jin123@gmail.com>
2024-11-20 22:03:47 +0100

    manage restaurant form

--- backend/src/middleware/validation.ts
+++ backend/src/middleware/validation.ts
@@ -40,4 +40,10 @@ export const validateMyRestaurantRequest = [
     .not()
     .isEmpty()
     .withMessage("Cuisines array cannot be empty"),
+  body("menuItems").isArray().withMessage("Menu items must be an array"),
+  body("menuItems.*.name").notEmpty().withMessage("Menu item name is required"),
+  body("menuItems.*.price")
+    .isFloat({ min: 0 })
+    .withMessage("Menu item price is required and must be a positive number"),
+  handleValidationErrors,
 ];
--- backend/src/routes/MyRestaurantRoute.ts
+++ backend/src/routes/MyRestaurantRoute.ts
@@ -2,6 +2,7 @@ import express from "express";
 import multer from "multer";
 import MyRestaurantController from "../controllers/MyRestaurantController";
 import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyRestaurantRequest } from "../middleware/validation";
 
 const router = express.Router();
 
@@ -16,9 +17,10 @@ const upload = multer({
 // /api/my/restaurant
 router.post(
   "/",
+  upload.single("imageFile"),
+  validateMyRestaurantRequest,
   jwtCheck,
   jwtParse,
-  upload.single("imageFile"),
   MyRestaurantController.createMyRestaurant
 );
 
